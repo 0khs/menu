57c0705801543826c05e7b061eb6594b66b4b92b
0khs <imstupid1028@gmail.com>
2026-06-12 17:56:48 +0800

    library

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -14,6 +14,7 @@ LOCAL_LDFLAGS += -Wl,--gc-sections,--strip-all
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/imGui \
                     $(LOCAL_PATH)/App/Touch \
+                    $(LOCAL_PATH)/App/Utils \
                     $(LOCAL_PATH)/App \
                     $(LOCAL_PATH)/Dobby 
 
